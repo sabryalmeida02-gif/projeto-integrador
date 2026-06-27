9dc1cea2576d56b6fd3c30df60d1e341bd65fac7
sabryalmeida02-gif <sabry.almeida02@gmail.com>
2026-06-27 19:11:06 -0300

    primeiro commits

--- js/script.js
+++ js/script.js
@@ -17,10 +17,39 @@ const dadosSimulador = {
         };
 
 function mostrarImpacto(tipo, elementoBotao) {
-    const botoes = document.querySelectorAll('.sim-btn')
-    console.log(botoes)
-    botoes.forEach(btn => btn.classList.remove('active'))
     
+     const dadosSimulador = {
+    aco: {
+        valor: "-1.500 kg",
+        rotulo: "de emissões de CO₂",
+        descricao: "A reciclagem de uma tonelada de aço economiza recursos naturais e evita a queima de combustível fóssil necessária para fundir novo minério."
+    },
+    niobio: {
+        valor: "-20%",
+        rotulo: "de peso estrutural",
+        descricao: "Pequenas porções de nióbio tornam o aço incrivelmente mais forte. Estruturas usam menos material e carros ficam leves, consumindo menos combustível."
+    },
+    energia: {
+        valor: "100%",
+        rotulo: "de Matriz Energética Limpa",
+        descricao: "Trocar fornos a carvão por matrizes eólicas e solares elimina a emissão direta de gases de efeito estufa na atmosfera no processo de manufatura."
+    }
+};
+
+function mostrarImpacto(tipo, elementoBotao) {
+
+    document.querySelectorAll(".sim-btn").forEach(btn => {
+        btn.classList.remove("active");
+    });
+
+    elementoBotao.classList.add("active");
+
+    const dados = dadosSimulador[tipo];
+
+    document.getElementById("metrica-valor").innerText = dados.valor;
+    document.getElementById("metrica-rotulo").innerText = dados.rotulo;
+    document.getElementById("metrica-descricao").innerText = dados.descricao;
+}
     elementoBotao.classList.add('active');
 
     document.getElementById('metrica-valor').innerText = dadosSimulador[tipo].valor
